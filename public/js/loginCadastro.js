2923305a39548395bc0801b4b29aa4da3a177868
calacee <felipecalacemiranda@gmail.com>
2024-11-26 00:22:54 -0300

    adicionando login e cadastro

---
+++ public/js/loginCadastro.js
@@ -0,0 +1,18 @@
+const loginBtn = document.getElementById("login-btn");
+const registerBtn = document.getElementById("register-btn");
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+
+loginBtn.addEventListener("click", () => {
+    loginBtn.classList.add("active");
+    registerBtn.classList.remove("active");
+    loginForm.classList.add("active");
+    registerForm.classList.remove("active");
+});
+
+registerBtn.addEventListener("click", () => {
+    registerBtn.classList.add("active");
+    loginBtn.classList.remove("active");
+    registerForm.classList.add("active");
+    loginForm.classList.remove("active");
+});
